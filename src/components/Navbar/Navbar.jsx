b7c4ab12a179aaaf625e011b9639709db8cf963f
amnipriyasowrirajan <amnipriya31@gmail.com>
2023-10-21 06:14:56 -0400

    first commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,55 +1,75 @@
-import React, { useState} from 'react';
-import {GiKitchenScale } from 'react-icons/gi';
-import { MdKitchen } from 'react-icons/md';
+import React, { useState } from "react";
+import { GiKitchenScale } from "react-icons/gi";
+import { MdKitchen } from "react-icons/md";
 
-import images from '../../constants/images';
-import './Navbar.css';
+import images from "../../constants/images";
+import "./Navbar.css";
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
 
-  const[toggleMenu, setToggleMenu] = useState(false);
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.mb_logo} alt="app logo" />
+      </div>
+      <ul className="app__navbar-links">
+        <li className="p__opensans">
+          <a href="#home">Home</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#about">About</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#kitchengallery">Gallery</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#video">Highlights</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
 
-  return(
-  <nav className='app__navbar' >
-<div className='app__navbar-logo'>
-  <img src={images.mb_logo} alt='app logo' />
-</div>
-<ul className='app__navbar-links'>
-  <li className='p__opensans'><a href="#home">Home</a></li>
-  <li className='p__opensans'><a href="#about">About</a></li>
-  <li className='p__opensans'><a href="#kitchengallery">Gallery</a></li>
-  <li className='p__opensans'><a href="#video">Highlights</a></li>
-  <li className='p__opensans'><a href="#contact">Contact</a></li>
+      <div className="app__navbar-smallscreen">
+        <GiKitchenScale
+          color="#fff"
+          fontSize={27}
+          onClick={() => setToggleMenu(true)}
+        />
 
-</ul>
-
-
-<div className='app__navbar-smallscreen'>
-  <GiKitchenScale color="#fff" fontSize={27} 
-  onClick={()=> setToggleMenu(true)}/>
- 
-{toggleMenu && (
-  <div className='app__navbar-smallscreen_overlay 
-  flex__center slide-bottom'>
-
-  <MdKitchen fontSize={27}
-   className='overlay__close' onClick={()=> setToggleMenu(false)} />
-  <ul className='app__navbar-smallscreen_links'>
-  <li className='p__opensans'><a href="#home">Home</a></li>
-  <li className='p__opensans'><a href="#about">About</a></li>
-  <li className='p__opensans'><a href="#kitchengallery">Gallery</a></li>
-  <li className='p__opensans'><a href="#video">Highlights</a></li>
-  <li className='p__opensans'><a href="#contact">Contact</a></li>
-
-  </ul>
- </div>
-
-  )}
-  
-</div>
-   
-  </nav >
-  )
-}
+        {toggleMenu && (
+          <div
+            className="app__navbar-smallscreen_overlay 
+  flex__center slide-bottom"
+          >
+            <MdKitchen
+              fontSize={27}
+              className="overlay__close"
+              onClick={() => setToggleMenu(false)}
+            />
+            <ul className="app__navbar-smallscreen_links">
+              <li className="p__opensans">
+                <a href="#home">Home</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#about">About</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#kitchengallery">Gallery</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#video">Highlights</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
